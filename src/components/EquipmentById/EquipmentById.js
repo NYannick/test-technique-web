85644f532cf981f90451ec0e81edeeadbe60eafa
Yannick Noël <>
2019-11-02 10:50:29 +0100

    Fix on refresh show data from checkpoints

--- src/components/EquipmentById/EquipmentById.js
+++ src/components/EquipmentById/EquipmentById.js
@@ -31,74 +31,68 @@ const EquipmentById = props => {
     let { id } = useParams()
     const [equipment, setEquipment] = useState([])
     const [checkpoints, setCheckpoints] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
 
     const classes = useStyles()
 
     useEffect(() => {
-        setIsLoading(true)
-        // if (Object.entries(props.equipments).length === 0 && props.equipments.constructor === Object) {
-            props.fetchEquipmentById(id)
-            props.fetchCheckpointsByIdEquipment(id)
-        // }
+        props.fetchEquipmentById(id)
+        props.fetchCheckpointsByIdEquipment(id)
     }, [])
 
     useEffect(() => {
-        setCheckpoints(props.checkpointsByIdEquipment)
-    }, [checkpoints])
-
-    useEffect(() => {
-        setEquipment(props.equipmentById)
-        return () => {
-            setIsLoading(false)
-        }
-    }, [equipment])
+        setEquipment(renderEquipment())
+        setCheckpoints(renderCheckpoints())
+    }, [equipment, props.checkpointsByIdEquipment])
 
     const renderEquipment = () => {
-        return (
-            <div className="container-btn-data">
-                <Link to="/" className="link-back">
-                    <Button
-                        variant="contained"
-                        className={classes.button}
-                        startIcon={<ArrowBack />}
-                    >
-                        Retour
-                    </Button>
-                </Link>
-                {
-                    isLoading
-                        ? <CircularProgress className={classes.progress} />
-                        : !_.isEmpty(props.equipmentById)
-                            ? (
-                                <Card>
-                                    <div className="container">
-                                        <div className="img-container"><SimpleImg src={props.equipmentById.photo} height={400} className="img-beeldi-detail" /></div>
-                                        <div className="text-container">
-                                            <p><span className="bold">Nom de l'équipement: </span>{props.equipmentById.name}</p>
-                                            <p><span className="bold">Domaine technique: </span>{props.equipmentById.domain}</p>
-                                            <p><span className="bold">Nombre de défauts sur l'équipement: </span>{props.equipmentById.nbFaults}</p>
-                                            <p><span className="bold">Marque: </span>{props.equipmentById.brand}</p>
-                                            <p><span className="bold">Modèle: </span>{props.equipmentById.model}</p>
-                                            <p><span className="bold">Dernier statut constaté: </span>{props.equipmentById.status}</p>
-                                            <p className="p-flex-column"><span className="bold marginBottom">Prise de notes: </span><span>{props.equipmentById.notes}</span></p>
-                                            <div className="container-img-beeldi-min">
-                                                {
-                                                    !_.isEmpty(props.checkpointsByIdEquipment)
-                                                        ? _.map(checkpoints, (checkpoint, key) => checkpoint.photo ? <Tooltip title={checkpoint.name ? checkpoint.name : null} key={key}><Card classes={{ root: classes.card }}><SimpleImg src={checkpoint.photo} height={105} className="img-beeldi-min" /></Card></Tooltip> : null)
-                                                        : <h4>No checkpoint(s) available</h4>
-                                                }
-                                            </div>
-                                        </div>
-                                    </div>
-                                </Card>
-                            ) : <h4>No equipment available</h4>
-                }
-            </div>
-        )
+        if (!_.isEmpty(props.equipmentById)) {
+            return props.equipmentById
+        }
+        return null
     }
 
-    return renderEquipment()
+    const renderCheckpoints = () => {
+        if (!_.isEmpty(props.checkpointsByIdEquipment)) {
+            return _.map(props.checkpointsByIdEquipment, (checkpoint, key) => checkpoint.photo ? <Tooltip title={checkpoint.name ? checkpoint.name : null} key={key}><Card classes={{ root: classes.card }}><SimpleImg src={checkpoint.photo} height={105} className="img-beeldi-min" /></Card></Tooltip> : null)
+        }
+        return <h4>No checkpoint(s) available</h4>
+    }
+
+    return (
+        <div className="container-btn-data">
+            <Link to="/" className="link-back">
+                <Button
+                    variant="contained"
+                    className={classes.button}
+                    startIcon={<ArrowBack />}
+                >
+                    Retour
+                </Button>
+            </Link>
+            {
+                equipment
+                    ? (
+                        <Card>
+                            <div className="container">
+                                <div className="img-container"><SimpleImg src={equipment.photo} height={400} className="img-beeldi-detail" /></div>
+                                <div className="text-container">
+                                    <p><span className="bold">Nom de l'équipement: </span>{equipment.name}</p>
+                                    <p><span className="bold">Domaine technique: </span>{equipment.domain}</p>
+                                    <p><span className="bold">Nombre de défauts sur l'équipement: </span>{equipment.nbFaults}</p>
+                                    <p><span className="bold">Marque: </span>{equipment.brand}</p>
+                                    <p><span className="bold">Modèle: </span>{equipment.model}</p>
+                                    <p><span className="bold">Dernier statut constaté: </span>{equipment.status}</p>
+                                    <p className="p-flex-column"><span className="bold marginBottom">Prise de notes: </span><span>{equipment.notes}</span></p>
+                                    <div className="container-img-beeldi-min">
+                                        {checkpoints}
+                                    </div>
+                                </div>
+                            </div>
+                        </Card>
+                    ) : <CircularProgress className={classes.progress} />
+            }
+        </div>
+    )
 }
 
 const mapStateToProps = ({ equipmentById, checkpointsByIdEquipment }) => {
